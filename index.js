625f2140e7eda92569bdd8760a5a475dde2a6e3e
Will <58743949+GreggsOfficial@users.noreply.github.com>
2020-04-08 20:02:44 +0100

    Initial commit
    Changed default settings to some relevant ones.

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
-module.exports = Franz => class CustomWebsite extends Franz {
-  async validateUrl(url) {
-    try {
-      const resp = await window.fetch(url, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-      return resp.status.toString().startsWith('2') || resp.status.toString().startsWith('3');
-    } catch (err) {
-      console.error(err);
-    }
-
-    return false;
+module.exports = Franz => class Messenger extends Franz {
+  overrideUserAgent() {
+    return window.navigator.userAgent.replace(/(Franz|Electron)([^\s]+\s)/g, '');
   }
 };
